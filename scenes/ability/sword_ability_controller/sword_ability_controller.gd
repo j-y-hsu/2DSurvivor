293fb7c5080a94fb872193d53d2cb59990bc23cb
Jake Hsu <jakeyhsu@gmail.com>
2023-12-04 14:58:47 -0500

    Added basic enemies, sword ability, and upgrade

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -4,10 +4,15 @@ const MAX_RANGE = 150
 
 @export var sword_ability: PackedScene
 
+var damage = 5
+var base_wait_time
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	base_wait_time = $Timer.wait_time
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 func on_timer_timeout():
@@ -28,7 +33,23 @@ func on_timer_timeout():
 		return a_distance < b_distance
 	)
 	
-	var sword_instance = sword_ability.instantiate() as Node2D
+	var sword_instance = sword_ability.instantiate() as SwordAbility
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = enemies[0].global_position
+	sword_instance.hitbox_component.damage = damage
 	
+	sword_instance.global_position = enemies[0].global_position
+	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
+	
+	var enemy_direction = enemies[0].global_position - sword_instance.global_position
+	sword_instance.rotation = enemy_direction.angle()
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id != "sword_rate":
+		return
+	
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * 0.1
+	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+	$Timer.start()
+	
+	print($Timer.wait_time)
